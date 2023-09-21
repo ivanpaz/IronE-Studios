0efdec1e4027afacf133ff6f7226e43d8294287a
ivanpaz <ivan.lucas.paz@gmail.com>
2023-09-20 22:04:27 -0300

    Update ScriptTeste.cs

--- Iron-E Studios Git/Assets/Scripts/ScriptTeste.cs
+++ Iron-E Studios Git/Assets/Scripts/ScriptTeste.cs
@@ -7,7 +7,7 @@ public class ScriptTeste : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Opa-lala");
     }
 
     // Update is called once per frame
